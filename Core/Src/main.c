a83ecf671ef52fd2788225da03f75919939db1ea
ryan <niemiryan@gmail.com>
2025-04-07 13:02:59 -0700

    slimmed to base config

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,71 +1,135 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "menu.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-UART_HandleTypeDef hlpuart1;
-CRC_HandleTypeDef CrcHandle;
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-extern pFunction JumpToApplication;
-extern uint32_t JumpAddress;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-static void IAP_Init(void);
 void SystemClock_Config(void);
-void Error_Handler(void);
-static void LED_Init(void);
-static void Button_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
 
 /**
-  * @brief  Main program
-  * @param  None
-  * @retval None
+  * @brief  The application entry point.
+  * @retval int
   */
-int main(void)
-{
-  /* STM32L4xx HAL library initialization */
+unsigned char BinaryCount(unsigned char count) {
+  // 
+	const unsigned long int 0xFFFFFFFF;
+	/* 0xFFFFFFFF is maximum value for an unsigned long int
+		 acts as a large value to count to, to spend time on execution cycles
+		 such that a human can see the led light up
+	*/
+	const unsigned char countTo = 15; // To binary count through 4 LEDs, 16 states are needed (15 + 0 = 16)
+	if (count == countTo) {
+		count = 0; // cycle back to 0 after reaching maximum value to count to
+	}
+	else {
+		count++; // count up until maximum value is reached
+	}
+	return count;
+}
+
+void LedAssign(unsigned char count) {
+	const unsigned char = maskFirstLED = 0b00000001;
+	const unsigned char = maskSecondLED = maskFirstLED >> 1;
+	const unsigned char = maskThirdLED = maskFirstLED >> 2;
+	const unsigned char = maskFourthLED = maskFirstLED >> 3;
+
+	if (maskFirstLED & count) == maskFirstLED {
+		GPIOC->BSSR = (GPIO_PIN_1); // turns on LED 1
+	}
+	else {
+		GPIOC->BRR = (GPIO_PIN_1); // turns off LED 1
+	}
+	
+	if (maskSecondLED & count) == maskSecondLED {
+		GPIOC->BSSR = (GPIO_PIN_2); //turns on LED 2
+	}
+	else {
+		GPIOC->BRR = (GPIO_PIN_2); // turns off LED 2
+	}
+
+	if (maskThirdLED & count) == maskThirdLED {
+		GPIOC->BSSR = (GPIO_PIN_3); //turns on LED 3
+	}
+	else {
+		GPIOC->BRR = (GPIO_PIN_3); // turns off LED 3
+	}
+
+	if (maskFourthLED & count) == maskFourthLED {
+		GPIOC->BSSR = (GPIO_PIN_4); //turns on LED 4
+	}
+	else {
+		GPIOC->BRR = (GPIO_PIN_4); // turns off LED 4
+	}
+}
+
+int main(void) {
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* Configure the system clock to 80 MHz */
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
   SystemClock_Config();
 
-  /* Initialize LED and Button */
-  void LED_Init(void);
-  void Button_Init(void);
+  /* USER CODE BEGIN SysInit */
 
-  /* Test if Button (PC13) is pressed */
-  if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_SET)
-  {
-    /* Initialize Flash */
-    FLASH_If_Init();
-    /* Execute the IAP driver in order to reprogram the Flash */
-    IAP_Init();
-    /* Display main menu */
-    Main_Menu();
+  /* USER CODE END SysInit */
 
-    /* Blink LED (PB7) */
-    while (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_SET)
-    {
-      HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
-      HAL_Delay(500); // Delay 500 ms
-    }
-  }
-  else
-  {
-    /* Test if user code is programmed starting from address "APPLICATION_ADDRESS" */
-    if (((*(__IO uint32_t*)APPLICATION_ADDRESS) & 0x2FFE0000 ) == 0x20000000)
-    {
-      /* Jump to user application */
-      JumpAddress = *(__IO uint32_t*) (APPLICATION_ADDRESS + 4);
-      JumpToApplication = (pFunction) JumpAddress;
-      /* Initialize user application's Stack Pointer */
-      __set_MSP(*(__IO uint32_t*) APPLICATION_ADDRESS);
-      JumpToApplication();
-    }
-  }
+  /* Initialize all configured peripherals */
+  /* USER CODE BEGIN 2 */
 
-  while (1)
-  {}
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  /* USER CODE END 3 */
+	unsigned char count = 0; // start counting from 0;
+	while(1) {
+		/*
+			counts in binary using 4 LEDs 
+		*/
+		LedAssign(count); // turns on and off appropriate LEDs
+		count = BinaryCount(count); // jump to next count with a time delay in between
+	}
 }
 
 /**
@@ -102,16 +166,11 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -121,90 +180,35 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
     Error_Handler();
   }
 }
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
 
 /**
-  * @brief  Initialize the IAP: Configure USART.
-  * @param  None
+  * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void IAP_Init(void)
-{
-  /* USART resources configuration (Clock, GPIO pins, and USART registers) */
-  /* USART configured as follows:
-        - BaudRate = 115200 baud
-        - Word Length = 8 Bits
-        - One Stop Bit
-        - No parity
-        - Hardware flow control disabled (RTS and CTS signals)
-        - Receive and transmit enabled
-  */
-  hlpuart1.Instance = LPUART1;
-  hlpuart1.Init.BaudRate = 115200;
-  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
-  hlpuart1.Init.StopBits = UART_STOPBITS_1;
-  hlpuart1.Init.Parity = UART_PARITY_NONE;
-  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  hlpuart1.Init.Mode = UART_MODE_RX | UART_MODE_TX;
-  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_RXOVERRUNDISABLE_INIT;
-  hlpuart1.AdvancedInit.OverrunDisable = UART_ADVFEATURE_OVERRUN_DISABLE;
-
-  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
-  {
-    /* Initialization Error */
-    Error_Handler();
-  }
-
-  __HAL_RCC_CRC_CLK_ENABLE();
-
-  /*##-2- Configure the CRC peripheral #####################################*/
-  CrcHandle.Instance = CRC;
-
-  /* The CRC-16-CCIT polynomial is used */
-  CrcHandle.Init.DefaultPolynomialUse    = DEFAULT_POLYNOMIAL_DISABLE;
-  CrcHandle.Init.GeneratingPolynomial    = 0x1021;
-  CrcHandle.Init.CRCLength               = CRC_POLYLENGTH_16B;
-
-  /* The zero init value is used */
-  CrcHandle.Init.DefaultInitValueUse     = DEFAULT_INIT_VALUE_DISABLE;
-  CrcHandle.Init.InitValue               = 0;
-
-  /* The input data are not inverted */
-  CrcHandle.Init.InputDataInversionMode  = CRC_INPUTDATA_INVERSION_NONE;
-
-  /* The output data are not inverted */
-  CrcHandle.Init.OutputDataInversionMode = CRC_OUTPUTDATA_INVERSION_DISABLE;
-
-  /* The input data are 32-bit long words */
-  CrcHandle.InputDataFormat              = CRC_INPUTDATA_FORMAT_BYTES;
-
-  if (HAL_CRC_Init(&CrcHandle) != HAL_OK)
-  {
-
-    while (1)
-    {}
-  }
-}
-
 void Error_Handler(void)
 {
-
-  while(1)
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
   {
-
   }
+  /* USER CODE END Error_Handler_Debug */
 }
-
-
 
 #ifdef USE_FULL_ASSERT
 /**
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Upon cloning repository, change all mentions of bootloaderfinal to your projects name. Ex:
 git clone https://github.com/stuntangel/stm32_template test_name
 mv bootloaderfinal.ioc test_name.ioc
+STM32_Programmer_CLI -l usb
+note to self: use the usb port on the right side of my computer! /dev/ttyACM0
+It is necessary to add user to dialout group in linux
+Flash: DISPLAY=:1 steam-run /home/ryan/STMicroelectronics/STM32Cube/STM32CubeProgrammer/bin/STM32CubeProgrammer
